53e46baba815516351640840ae3bdea9df3db62b
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2025-06-25 15:41:26 +0530

    updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1402500176"
+    serviceWorkerVersion: "2302928354"
   }
 });
